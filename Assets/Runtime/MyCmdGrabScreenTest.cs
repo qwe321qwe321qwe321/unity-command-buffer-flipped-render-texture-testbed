534233cfef1571e7e511ce196d36b2e7b548d061
qwe321qwe321qwe321 <aegis321123@gmail.com>
2024-05-23 18:28:02 +0800

    build

--- Assets/Runtime/MyCmdGrabScreenTest.cs
+++ Assets/Runtime/MyCmdGrabScreenTest.cs
@@ -11,12 +11,14 @@ public class MyCmdGrabScreenTest : MonoBehaviour
 {
     public CameraEvent insertCommandBufferEvent = CameraEvent.AfterImageEffects;
     public BuiltinRenderTextureType sourceRenderTextureType = BuiltinRenderTextureType.CurrentActive;
-    [SerializeField] private Material m_BlitMaterial;
+    public bool useCustomBlit = true;
+    [SerializeField] private Material m_CustomBlitMaterial;
     [SerializeField] private RawImage m_RawImage;
     [SerializeField] private RenderTexture m_RenderTexture;
     [SerializeField] private PostProcessLayer m_PostProcessLayer;
 
-    [SerializeField] private Camera m_Camera;
+    [SerializeField] private Camera m_TargetCamera;
+    [SerializeField] private Camera m_SecondCamera;
     
     private CommandBuffer m_CommandBuffer;
     private CameraEvent m_LastInsertEvent;
@@ -25,15 +27,15 @@ public class MyCmdGrabScreenTest : MonoBehaviour
         ValidateRenderTexture();
         SetUpCommandBuffer();
         if (m_LastInsertEvent != insertCommandBufferEvent) {
-            m_Camera.RemoveCommandBuffer(m_LastInsertEvent, m_CommandBuffer);
-            m_Camera.AddCommandBuffer(insertCommandBufferEvent, m_CommandBuffer);
+            m_TargetCamera.RemoveCommandBuffer(m_LastInsertEvent, m_CommandBuffer);
+            m_TargetCamera.AddCommandBuffer(insertCommandBufferEvent, m_CommandBuffer);
             m_LastInsertEvent = insertCommandBufferEvent;
         }
     }
 
     private void OnDisable() {
         if (m_LastInsertEvent != default && m_CommandBuffer != null) {
-            m_Camera.RemoveCommandBuffer(m_LastInsertEvent, m_CommandBuffer);
+            m_TargetCamera.RemoveCommandBuffer(m_LastInsertEvent, m_CommandBuffer);
         }
     }
 
@@ -48,8 +50,8 @@ public class MyCmdGrabScreenTest : MonoBehaviour
         }
         m_CommandBuffer.Clear();
         var dest = m_RenderTexture;
-        if (m_BlitMaterial) {
-            m_CommandBuffer.Blit(sourceRenderTextureType, dest, m_BlitMaterial, 0);
+        if (useCustomBlit) {
+            m_CommandBuffer.Blit(sourceRenderTextureType, dest, m_CustomBlitMaterial, 0);
         } else {
             m_CommandBuffer.Blit(sourceRenderTextureType, dest);
         }
@@ -60,19 +62,19 @@ public class MyCmdGrabScreenTest : MonoBehaviour
         if (m_RenderTexture && m_RenderTexture.IsCreated()) {
             return;
         }
-        
-        var descriptor = new RenderTextureDescriptor(Screen.width, Screen.height, GraphicsFormat.R8G8B8A8_SRGB,
-            GraphicsFormat.None, 0) {
-            depthBufferBits = 0,
-            bindMS = false,
-            msaaSamples = 1,
-            graphicsFormat = GraphicsFormat.R8G8B8A8_SRGB,
-            colorFormat = RenderTextureFormat.ARGB32,
-            depthStencilFormat = GraphicsFormat.None,
-            dimension = TextureDimension.Tex2D,
-            volumeDepth = 1,
-            shadowSamplingMode = ShadowSamplingMode.None,
-        };
+
+        var descriptor = new RenderTextureDescriptor(Screen.width, Screen.height, RenderTextureFormat.Default, 0);
+        // {
+        //     depthBufferBits = 0,
+        //     bindMS = false,
+        //     msaaSamples = 1,
+        //     graphicsFormat = GraphicsFormat.R8G8B8A8_SRGB,
+        //     colorFormat = RenderTextureFormat.ARGB32,
+        //     depthStencilFormat = GraphicsFormat.None,
+        //     dimension = TextureDimension.Tex2D,
+        //     volumeDepth = 1,
+        //     shadowSamplingMode = ShadowSamplingMode.None,
+        // };
         
         m_RenderTexture = new RenderTexture(descriptor);
         m_RenderTexture.name = "ScreenRT (descriptor)";
@@ -84,6 +86,7 @@ public class MyCmdGrabScreenTest : MonoBehaviour
     private Vector2 m_GuiVerticalScrollPosition;
     private void OnGUI() {
         const float PaddingY = 30f;
+        const float Indent = 10f;
         Rect scrollRect = new Rect(0f, 0f, 300f, Screen.height);
         GUI.Box(scrollRect, GUIContent.none);
         m_GuiVerticalScrollPosition = GUI.BeginScrollView(
@@ -92,6 +95,18 @@ public class MyCmdGrabScreenTest : MonoBehaviour
             new Rect(0f, 0f, 200f, 2000f)
         );
         Rect rect = new Rect(20, 20f, 300f, PaddingY);
+        GUI.Label(rect, $"Graphics: {SystemInfo.graphicsDeviceType}");
+        rect.y += PaddingY;
+        if (m_RenderTexture) {
+            GUI.Label(rect, $"RT Format: {m_RenderTexture.format}");
+            rect.y += PaddingY;
+            GUI.Label(rect, $"graphicsFormat: {m_RenderTexture.graphicsFormat}");
+            rect.y += PaddingY;
+            GUI.Label(rect, $"depthStencilFormat: {m_RenderTexture.depthStencilFormat}");
+            rect.y += PaddingY;
+
+        }
+        
         EnumToggleGroup(ref rect, PaddingY, ref insertCommandBufferEvent,
             CameraEvent.BeforeForwardOpaque,
             CameraEvent.AfterForwardOpaque,
@@ -105,19 +120,65 @@ public class MyCmdGrabScreenTest : MonoBehaviour
             BuiltinRenderTextureType.CameraTarget,
             BuiltinRenderTextureType.CurrentActive
         );
+        // Blit Material.
+        useCustomBlit = GUI.Toggle(rect, useCustomBlit, "Custom Blit Shader");
+        rect.y += PaddingY;
+        
         MSAAToggle(ref rect, PaddingY, 0, "No MSAA");
         MSAAToggle(ref rect, PaddingY, 2, "MSAA 2x");
         MSAAToggle(ref rect, PaddingY, 4, "MSAA 4x");
         MSAAToggle(ref rect, PaddingY, 8, "MSAA 8x");
+        
         // HDR.
-        m_Camera.allowHDR = GUI.Toggle(rect, m_Camera.allowHDR, "Allow HDR");
+        m_TargetCamera.allowHDR = GUI.Toggle(rect, m_TargetCamera.allowHDR, "Allow HDR");
         rect.y += PaddingY;
         
         // Post Process.
         m_PostProcessLayer.enabled = GUI.Toggle(rect, m_PostProcessLayer.enabled, "Post Process");
         rect.y += PaddingY;
         
+        // forceIntoRenderTexture.
+        m_TargetCamera.forceIntoRenderTexture = GUI.Toggle(rect, m_TargetCamera.forceIntoRenderTexture, "forceIntoRenderTexture");
+        rect.y += PaddingY;
+        
+        // Second Camera.
+        SecondCameraOptions(ref rect, PaddingY, Indent);
+        
         GUI.EndScrollView();
+    }
+
+    private void SecondCameraOptions(ref Rect rect, float paddingY, float indent) {
+        GUI.Label(rect, "Second Camera:");
+        rect.y += paddingY;
+        rect.x += indent;
+        
+        m_SecondCamera.enabled = GUI.Toggle(rect, m_SecondCamera.enabled, "enabled");
+        rect.y += paddingY;
+        
+        // Second Camera's viewport rect.
+        Rect FullscreenViewport = new Rect(0f, 0f, 1f, 1f);
+        Rect NonFullscreenViewport = new Rect(0f, 0f, 0.5f, 0.5f);
+        bool isFullscreenViewport = m_SecondCamera.rect == FullscreenViewport;
+        isFullscreenViewport = GUI.Toggle(rect, isFullscreenViewport, "viewport: Fullscreen");
+        if (isFullscreenViewport && m_SecondCamera.rect != FullscreenViewport) {
+            m_SecondCamera.rect = FullscreenViewport;
+        } else if (!isFullscreenViewport && m_SecondCamera.rect != NonFullscreenViewport) {
+            m_SecondCamera.rect = NonFullscreenViewport;
+        }
+        rect.y += paddingY;
+        
+        // Second Camera's depth.
+        const float PriorDepth = -10f;
+        const float NotPriorDepth = 10f;
+        bool secondCameraPrior = m_SecondCamera.depth == PriorDepth;
+        secondCameraPrior = GUI.Toggle(rect, secondCameraPrior, "Prior to MainCamera");
+        if (secondCameraPrior && m_SecondCamera.depth != PriorDepth) {
+            m_SecondCamera.depth = PriorDepth;
+        } else if (!secondCameraPrior && m_SecondCamera.depth != NotPriorDepth) {
+            m_SecondCamera.depth = NotPriorDepth;
+        }
+
+        rect.x -= indent;
     }
 
     private static void EnumToggleGroup<T>(ref Rect rect, float padding, ref T variable, params T[] options) where T : System.Enum {
